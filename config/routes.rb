72adca5ef721e91a3832907ce5a14e7546c8eab4
willian <willian.l.lucio@gmail.com>
2018-11-21 16:25:06 -0200

    Creating dashboard.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
   devise_for :users
+
+  authenticated :user do
+    root to: "dashboard#index", as: :authenticated_root
+  end
+
+  unauthenticated :user do
     root to: "home#index"
+  end
 end
