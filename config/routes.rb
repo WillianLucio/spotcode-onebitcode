03581e0221393c3494e482b74a97797e101539b0
willian <willian.l.lucio@gmail.com>
2018-11-21 17:25:00 -0200

    Search page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'dashboard/index'
   devise_for :users
 
   authenticated :user do
     root to: "dashboard#index", as: :authenticated_root
+    resources :search, only: [:index, :new], as: :searches
   end
 
   unauthenticated :user do
